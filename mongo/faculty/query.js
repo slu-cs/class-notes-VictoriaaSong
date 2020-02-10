4b1f8ec31f3d1a093a9dc535844f010549b44665
Yu Song <ysong17@stlawu.edu>
2020-02-10 13:52:49 -0500

    First Mongodb query

---
+++ mongo/faculty/query.js
@@ -0,0 +1,15 @@
+// Store some data in the faculty database
+
+const mongoose = require('mongoose');
+const connect = require('./db');
+const Professor = require('./schema');
+
+connect();  // To the database
+
+// What documents are in the collection?
+const query = Professor.find();
+query.exec(function(error, professors) {
+  if(error) console.error(error.stack);
+
+  console.log(professors);
+});
