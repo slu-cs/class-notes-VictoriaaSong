c7fafae53d626876dedd6d69af16fccdd3a9f27d
Yu Song <ysong17@stlawu.edu>
2020-02-12 13:52:54 -0500

    Close connection after promises

--- mongo/faculty/query.js
+++ mongo/faculty/query.js
@@ -36,6 +36,18 @@ const queries = [
 
 ];
 
+// Run the queries in parallel
+Promise.all(queries)
+  .then(function(results) {
+    console.log('Names in order: ', results[0].map(p => p.name));
+    console.log('Started most recently: ', results[1].map(p => p.name));
+    console.log('Started in 2003: ', results[2].map(p => p.name));
+    console.log('Teaches 362: ', results[3].map(p => p.name));
+    console.log('Distinct ranks: ', results[4]);
+    mongoose.connection.close();
+ }).catch(error => console.error(error.stack));
+
+/*
 // Professor.find().sort('name')
 queries[0].exec(function(error, professors){
   if(error) console.error(error.stack);
@@ -71,3 +83,4 @@ queries[4].exec(function(error, ranks){
   if(error) console.error(error.stack);
   console.log('Distinct ranks: ', ranks);
 });
+*/
