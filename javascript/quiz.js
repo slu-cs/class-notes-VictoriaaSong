cc688714474f573d30160775cc71da84f5a2455b
Victoria Song <songyu@VictoriadeMacBook-Pro.local>
2020-03-01 17:21:58 -0500

    3.1

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -22,7 +22,7 @@ labs.sort((a, b) => a.seats-b.seats);
 const numbers = [5, 4, 3, 2, 1];
 
 // A. Get the first even number (by invoking the built-in find method).
-const even = numbers.find(x => x % 2 < 1);
+const even = numbers.filter(x => x % 2 < 1);
 console.log(even);
 
 // B. Define a standalone find function. The array is its first argument and the callback is its second argument.
