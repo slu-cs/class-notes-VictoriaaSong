9d73707b7702aabfccf3f09ed14d24640e40263a
Yu Song <ysong17@stlawu.edu>
2020-03-04 13:35:51 -0500

    Express routes

--- express/hello/app.js
+++ express/hello/app.js
@@ -4,11 +4,30 @@ const express = require('express');
 // Create the server
 const app = express();
 
-// Respond to one request
+// cs-linuxlab-18.stlawu.edu:3000/
 app.get('/', function(request, response){
   response.send('Hello Victoria');
 });
 
+// cs-linuxlab-18.stlawu.edu:3000/foo
+app.get('/foo', function(request, response){
+  response.send('Hello foo');
+});
+
+// cs-linuxlab-18.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(request, response){
+  response.send('Hello bar');
+});
+
+// cs-linuxlab-18.stlawu.edu:3000/zap/z (for any z)
+app.get('/zap/:z', function(request, response){
+  response.send(`Hello ${request.params.z}`);
+});
+
+// cs-linuxlab-18.stlawu.edu:3000/zap?x=X&y=Y
+app.get('/zap', function(request, response){
+  response.send(`Hello zap with ${request.query.x} and ${request.query.x}`);
+});
 
 // Start the server
 app.listen(3000);  // 3000 is the port
