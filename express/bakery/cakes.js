9b870c7001bfa25c31d7ecbd56f0e3b72965b032
Yu Song <ysong17@stlawu.edu>
2020-03-09 13:48:52 -0400

    EJS includes and expressions

--- express/bakery/cakes.js
+++ express/bakery/cakes.js
@@ -4,6 +4,12 @@ const express = require('express');
 // Create the router
 const router = express.Router();
 
+// Pretend database collection
+const cakes = [
+  {id: 'vanilla', flavor: 'Vanilla'},
+  {id: 'chocolate', flavor: 'Chocolate'}
+]
+
 // cs-linuxlab-18.stlawu.edu:3000/cakes/
 router.get('/', function(request, response){
   response.send(`
@@ -17,12 +23,13 @@ router.get('/', function(request, response){
 
 // cs-linuxlab-18.stlawu.edu:3000/cakes/id
 router.get('/:id', function(request, response){
-  if(request.params.id === 'vanilla'){
-    response.send('Vanilla cakes');
-  }else if(request.params.id === 'chocolate'){
-    response.send('Chocolate cakes');
-  }else{
+  // Pretend database Lookup
+  const cake = cakes.find(cake => cake.id === request.params.id)
+
+  if(!cake){   // cake === undefined
     next();  // Pass on this request
+  }else{
+    response.render('cakes/detail', {cakes: cake});
   }
 });
 
