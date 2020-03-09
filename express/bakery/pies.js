d0dd071e1b9401396716bf9615e2a6ae8fa47058
Yu Song <ysong17@stlawu.edu>
2020-03-09 14:12:42 -0400

    Updates pies

--- express/bakery/pies.js
+++ express/bakery/pies.js
@@ -4,26 +4,26 @@ const express = require('express');
 // Create the router
 const router = express.Router();
 
-// cs-linuxlab-##.stlawu.edu:3000/pies/
-router.get('/', function(request, response) {
-response.send(`
-  <h1>Pies</h1>
-  <ul>
-    <li><a href="/pies/apple">Apple</a></li>
-    <li><a href="/pies/cherry">Cherry</a></li>
-  </ul>
-`);
+// Pretend database collection
+const cakes = [
+  {id: 'apple', flavor: 'Apple'},
+  {id: 'cherry', flavor: 'Cherry'}
+]
+
+// cs-linuxlab-18.stlawu.edu:3000/cakes/
+router.get('/', function(request, response){
+  response.render('pies/index', {pies: pies});
 });
 
-// cs-linuxlab-##.stlawu.edu:3000/pies/id
-router.get('/:id', function(request, response, next) {
-if (request.params.id === 'apple') {
-  response.send('Apple pies');
-} else if (request.params.id === 'cherry') {
-  response.send('Cherry pies');
-} else {
-  next(); // Pass on this request
-}
-});
+// cs-linuxlab-18.stlawu.edu:3000/cakes/id
+router.get('/:id', function(request, response){
+  // Pretend database Lookup
+  const pie = pies.find(pie => pie.id === request.params.id)
 
+  if(!pie){   // cake === undefined
+    next();  // Pass on this request
+  }else{
+    response.render('pies/detail', {pies: pie});
+  }
+});
 module.exports = router;
